a500115b22331d6e978bc5f0b071459268f938fe
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-07-16 13:04:12 +0900

    shopify inventory_item

---
+++ src/Models/Node/Source/Repository/Shopify/Rest/InventoryItemRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace HaruyaNishikubo\Transporter\Models\Node\Source\Repository\Shopify\Rest;
+
+use HaruyaNishikubo\Transporter\Models\Node\Collection\Shopify\Rest\InventoryItemCollection;
+use HaruyaNishikubo\Transporter\Models\Node\Collection\Shopify\Rest\VariantCollection;
+
+class InventoryItemRepository extends Repository
+{
+    protected int $inventory_item_id;
+
+    public function listUrl(): string
+    {
+        return sprintf('/inventory_items.json?ids=%d', $this->inventory_item_id);
+    }
+
+    public function prepare(): static
+    {
+        // collection
+        $this->collection = new InventoryItemCollection();
+
+        return $this;
+    }
+
+    protected function rootKey(): string
+    {
+        return 'inventory_items';
+    }
+
+    public function setAttributes(array $attributes): static
+    {
+        $this->inventory_item_id = $attributes['inventory_item_id'];
+
+        return $this;
+    }
+}
